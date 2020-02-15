47e5945c1ab729a502e56c39c4d88b4f2646a183
fatima <fmndako@gmail.com>
2020-02-15 19:41:26 +0100

    Eslint enabled

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,4 @@
-"use strict";
-const UserService = require('../services/users/users')   
+const UserService = require('../services/users/users');   
 
 class UserController{
     getUsers = async function (req, res, next) {
@@ -8,72 +7,72 @@ class UserController{
         var page = req.params.page ? req.params.page : 1;
         var limit = req.params.limit ? req.params.limit : 10;
         try {
-            var users = await UserService.getUsers({}, page, limit)
-            return res.status(200).json({ status: 200, data: users, message: "Succesfully Users Retrieved" });
+            var users = await UserService.getUsers({}, page, limit);
+            return res.status(200).json({ status: 200, data: users, message: 'Succesfully Users Retrieved' });
         } catch (error) {
-            res.processError(400, error)
+            res.processError(400, error);
 
         }
     }
     createUser = async function (req, res, next) {
         try {
-            console.log(req.body)
-            let user = await UserService.createUser(req.body)
-            const token = await user.generateAuthToken()
-            res.status(201).send({ user, token })
+            console.log(req.body);
+            let user = await UserService.createUser(req.body);
+            const token = await user.generateAuthToken();
+            res.status(201).send({ user, token });
         } catch (error) {
-            res.processError(400, error)
+            res.processError(400, error);
         }
     }
     getProfile = async function (req, res, next) {
         try{
-            res.send(req.user)
+            res.send(req.user);
         }
         catch(error){
-            res.processError(400, error)
+            res.processError(400, error);
         }
     }
     updateProfile = async function (req, res, next) {
         try {
-            let user = await UserService.updateUser(req.user._id, req.body)
-            res.status(201).send({user})
-    } catch (error) {
-        res.processError(400, error)
-    }
+            let user = await UserService.updateUser(req.user._id, req.body);
+            res.status(201).send({user});
+        } catch (error) {
+            res.processError(400, error);
+        }
     }
     login = async function (req, res, next) {
         try {
-            console.log(req.body)
-            const { email, password } = req.body
-            const user = await UserServices.getUserByCredentials(email, password)
+            console.log(req.body);
+            const { email, password } = req.body;
+            const user = await UserService.getUserByCredentials(email, password);
             if (!user) {
-                return res.processError(401, 'Login failed! Check authentication credentials')
+                return res.processError(401, 'Login failed! Check authentication credentials');
             }
-            const token = await user.generateAuthToken()
-            res.send({ user, token })
+            const token = await user.generateAuthToken();
+            res.send({ user, token });
         } catch (error) {
-            res.processError(400, "error")
+            res.processError(400, 'error');
         }     
     }
     logout = async function (req, res, next) {
         try {
             req.user.tokens = req.user.tokens.filter((token) => {
-                return token.token != req.token
-            })
-            await req.user.save()
-            res.send()
+                return token.token !== req.token;
+            });
+            await req.user.save();
+            res.send();
         } catch (error) {
-            res.processError(400, error)
+            res.processError(400, error);
         }
     }    
     logoutAll = async function (req, res, next){
-            try {
-                req.user.tokens.splice(0, req.user.tokens.length)
-                await req.user.save()
-                res.send()
-            } catch (error) {
-                res.processError(400, error)
-            }
+        try {
+            req.user.tokens.splice(0, req.user.tokens.length);
+            await req.user.save();
+            res.send();
+        } catch (error) {
+            res.processError(400, error);
+        }
     }
 }
 
--- middleware/isSeller.js
+++ middleware/isSeller.js
@@ -1,19 +1,19 @@
 
-const auth = require('./isAuthenticated')
+const auth = require('./isAuthenticated');
 
 const superadmin = async(req, res, next) => {
     try {
         auth(req, res, function(){
-            if(req.user.role === "merchant"){
-                next()
+            if(req.user.role === 'merchant'){
+                next();
             }
             else {
-             res.processError(401, "Token valid, but you don't have the right permission to access this resource");
+                res.processError(401, 'Token valid, but you don\'t have the right permission to access this resource');
             }
-        })
+        });
     } catch (error) {
-        res.processError(401, 'Not authorized to access this resource')
+        res.processError(401, 'Not authorized to access this resource');
     }
 
-}
-module.exports = superadmin
+};
+module.exports = superadmin;
