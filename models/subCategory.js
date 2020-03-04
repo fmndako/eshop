59d547234087fd30deebd2172af1594eccbb3547
mb02mn03 <mohammedlndako@gmail.com>
2020-03-04 20:47:09 +0100

    Deleted SubCategory Model

--- models/subCategory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const SubCategory = new Schema({
-    male: Boolean,
-    female: Boolean,
-});
-
-module.exports = mongoose.model('SubCategory', SubCategory);
